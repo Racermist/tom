a5834022e699cb887d5e07fc4186ba2169079a89
Racermist <99584207+Racermist@users.noreply.github.com>
2022-03-10 20:11:05 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var s1
+var s2
+function setup() 
+{
+  createCanvas(400, 400);
+  s1 = new Student("john",12,6);
+  s1.display()
+  s2 = new Student("Tom",13,9);
+  s2.display();
+}
+function draw() 
+{
+  background(51);
+ 
+}
+
+
